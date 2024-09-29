2493dda2d4d45e94b4af4831817f4ed61ba6cb13
Luis Chang <luis.chang@outlook.com>
2024-09-29 12:48:51 -0500

    Gallery Fragment with WebView

--- app/src/main/java/dev/luischang/navigationdrawer/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/luischang/navigationdrawer/ui/gallery/GalleryFragment.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import dev.luischang.navigationdrawer.databinding.FragmentGalleryBinding
+import dev.luischang.navigationdrawer.ui.shared.HomeGalleryViewModel
 
 class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
+    private val homeGalleryViewModel: HomeGalleryViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +27,27 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
-
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val wvPage: WebView = binding.wvPage
+        wvPage.settings.javaScriptEnabled = true
+
+        wvPage.webViewClient = object : WebViewClient (){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                url: String
+            ): Boolean {
+                if (url != null)
+                    view?.loadUrl(url)
+                return true
+            }
         }
+
+        homeGalleryViewModel.url.observe(viewLifecycleOwner) { url ->
+            wvPage.loadUrl(url)
+        }
+
         return root
     }
 
---
+++ app/src/main/java/dev/luischang/navigationdrawer/ui/shared/HomeGalleryViewModel.kt
@@ -0,0 +1,15 @@
+package dev.luischang.navigationdrawer.ui.shared
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeGalleryViewModel: ViewModel() {
+    private val _url = MutableLiveData<String>()
+    val url: LiveData<String> = _url
+
+    fun setUrl(url: String) {
+        _url.value = url
+    }
+
+}
